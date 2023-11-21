7dc4ac460e0d4bc3373a11d1340937a668eda99d
JoshuaBRussell <theruss007@gmail.com>
2023-11-20 22:13:10 -0500

    Add a templated (math) Vector class.

---
+++ src/Vector.cpp
@@ -0,0 +1,14 @@
+#include "Vector.hpp"
+
+/*
+template <unsigned int N>
+Vector<N> operator*(const double& s, Vector<N> vec){
+        Vector<N> result;
+        
+        for (unsigned int i = 0; i < N; i++){
+            result[i] = s*vec[i];
+        }
+
+        return result;
+}
+*/
