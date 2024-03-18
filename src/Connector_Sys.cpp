007e7e80998e35b98751a517b951059a2f01aba9
JoshuaBRussell <theruss007@gmail.com>
2024-03-17 20:58:56 -0400

    Refactor Connector System - to work correctly.

--- src/Connector_Sys.cpp
+++ src/Connector_Sys.cpp
@@ -21,36 +21,31 @@ void Connector_System(ECS_Manager &world){
     for (auto it = world.get_component_begin<Connector_Component>(); 
               it < world.get_component_end<Connector_Component>(); it++){
         
-        Eigen::Vector2f conn_pos = it->pos; 
-        Eigen::Vector2f conn_force = it->force; 
-        Eigen::Vector2f conn_torque = it->torque;  
+        Eigen::Vector2f conn_body_pos = it->pos; // Location of Connector Relative to Body CoG 
+        Eigen::Vector2f conn_force = it->force;  // Force Applied at Connector Body 
+        float conn_torque = it->torque;  
    
-        int attached_entity = it->attached_entity;
+        int attached_entity = it->attached_entity; // The Entity ID of the entity this connector is 
+                                                   // attached to
 
         Force_Component* force_comp_ptr = world.get_component<Force_Component>(attached_entity);
         Torque_Component* torque_comp_ptr = world.get_component<Torque_Component>(attached_entity);
         Rotation_Component* rot_comp_ptr = world.get_component<Rotation_Component>(attached_entity);
-
+        
+        // Transfer the connection force to the CoG
         force_comp_ptr->force += conn_force;
 
         // Transform force into body space from world space
-        Eigen::Rotation2D<float> transform_matr = Eigen::Rotation2D<float>((3.14159/180.0)*rot_comp_ptr->angle);
-
-        // Converting this to a 3D Cross Product Problem since it is mentally easier
-        conn_force = transform_matr * conn_force; 
-        Eigen::Vector3f force_3d = Eigen::Vector3f();
-        force_3d(0) = conn_force.x();
-        force_3d(1) = conn_force.y(); 
-        force_3d(2) = 0.0;
+        Eigen::Rotation2D<float> transform_matr = Eigen::Rotation2D<float>((-3.14159/180.0)*rot_comp_ptr->angle);
+        // Keeping this stale comment in b/c the fact that 'toRotatoinMatrix()' is needed is not 
+        // intuitively obvious.
+        // std::cout << transform_matr.toRotationMatrix() << std::endl;  
+        Eigen::Vector2f conn_force_body = transform_matr * conn_force; 
         
-        Eigen::Vector3f pos_3d = Eigen::Vector3f();
-        pos_3d(0) = conn_pos.x();
-        pos_3d(1) = conn_pos.y(); 
-        pos_3d(2) = 0.0;
-        std::cout << "Value: \n";
-        std::cout << pos_3d.cross(force_3d).norm() << std::endl;
-        torque_comp_ptr->torque += pos_3d.cross(force_3d).norm(); 
-
+        // Positive Torque is CCW
+        float torque = -(conn_force_body.x() * conn_body_pos.y() - conn_force_body.y() * conn_body_pos.x()); 
+        torque_comp_ptr->torque += torque;
+    
     }
 
 }
