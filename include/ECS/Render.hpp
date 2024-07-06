082749412c6ab88986e8ace609d131db280f354b
JoshuaBRussell <theruss007@gmail.com>
2024-07-06 18:42:15 -0400

    Remove these functions as they treat the Raylib Mouse as an object - however, in commit AEE606B of raylib-cpp, Mouse was changed to be a namespace rather than a class. These were no longer functional anyways.

--- include/ECS/Render.hpp
+++ include/ECS/Render.hpp
@@ -6,10 +6,6 @@
 #include <raylib-cpp.hpp>
 
 
-raylib::Vector2 Input_get_pos_from_mouse(raylib::Mouse &mouse_instance);
-bool Input_is_button_pressed(raylib::Mouse &mouse_instance, int button);
-bool Input_should_close();
-
 void Render_System_Exclusive(ECS_Manager &world);
 void Render_System_NonExclusive(ECS_Manager &world);
 void Render_System(ECS_Manager &world);
