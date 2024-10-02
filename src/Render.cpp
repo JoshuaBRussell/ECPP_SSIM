ead04e725b58a3b6a868ba55590f01cfcf884817
JoshuaBRussell <theruss007@gmail.com>
2024-10-01 21:14:43 -0400

    Remove dead code from the Render System.

--- src/Render.cpp
+++ src/Render.cpp
@@ -45,86 +45,6 @@ bool Render_System_WindowShouldClose(){
     return WindowShouldClose();
 }
 
-/*
-void Render_System_Exclusive(ECS_Manager &world){
-   
-    BeginDrawing();
-    ClearBackground(BLACK); 
-
-    for (auto it = world.get_component_begin<Position_Component>(); 
-              it < world.get_component_end<Position_Component>(); it++){
-        
-        // Check to see if the texture has been seen before
-        std::string tex_loc = world.get_component<Render_Component>(it->entity_id)->texture_loc;
-        if (texture_repo.find(tex_loc) == texture_repo.end()){
-            // Add it to the texture texture_repo
-            raylib::Texture2D* texture_ptr = new raylib::Texture2D(tex_loc);
-            texture_repo.insert({tex_loc, texture_ptr}); 
-        }
-        
-        Vector2D obj_pos = world.get_component<Position_Component>(it->entity_id)->position;
-        double obj_radius = world.get_component<Collision_Component>(it->entity_id)->radius;
-        
-        double obj_diameter = 2*obj_radius; 
-        
-        raylib::Texture2D *texture_ptr = texture_repo[tex_loc];
-        Vector2 tex_size = texture_ptr->GetSize();
-          
-        raylib::Rectangle src_rec(0.0, 0.0, tex_size.x, tex_size.y); // Use the entire texture size
-        raylib::Rectangle dest_rec(world2screen_X(obj_pos.x), world2screen_Y(obj_pos.y), 
-                                   world2screenscale_X(obj_diameter), world2screenscale_Y(obj_diameter));
-
-        //origin is relative to dest_rec
-        raylib::Vector2 origin = {world2screenscale_X(obj_diameter)/2, world2screenscale_Y(obj_diameter/2)};
-        texture_ptr->Draw(src_rec, dest_rec, origin, 0.0);
-            
-    }
- 
-    DrawFPS(10,10);
-
-    EndDrawing();  
-    
-}
-
-void Render_System_NonExclusive(ECS_Manager &world){
-   
-    for (auto it = world.get_component_begin<Position_Component>(); 
-              it < world.get_component_end<Position_Component>(); it++){
-        
-        // Check to see if the texture has been seen before
-        std::string tex_loc = world.get_component<Render_Component>(it->entity_id)->texture_loc;
-        if (texture_repo.find(tex_loc) == texture_repo.end()){
-            // Add it to the texture texture_repo
-            raylib::Texture2D* texture_ptr = new raylib::Texture2D(tex_loc);
-            texture_repo.insert({tex_loc, texture_ptr}); 
-        }
-        
-        Vector2D obj_pos = world.get_component<Position_Component>(it->entity_id)->position;
-        double obj_radius = world.get_component<Collision_Component>(it->entity_id)->radius;
-        
-        double obj_diameter = 2*obj_radius; 
-        
-        raylib::Texture2D *texture_ptr = texture_repo[tex_loc];
-        Vector2 tex_size = texture_ptr->GetSize();
-          
-        raylib::Rectangle src_rec(0.0, 0.0, tex_size.x, tex_size.y); // Use the entire texture size
-        raylib::Rectangle dest_rec(world2screen_X(obj_pos.x), world2screen_Y(obj_pos.y), 
-                                   world2screenscale_X(obj_diameter), world2screenscale_Y(obj_diameter));
-        
-        //origin is relative to dest_rec
-        raylib::Vector2 origin = {world2screenscale_X(obj_diameter)/2, world2screenscale_Y(obj_diameter/2)};
-        texture_ptr->Draw(src_rec, dest_rec, origin, 0.0);
-
-        //raylib::Text ID_str(std::to_string(it->entity_id), 16.0, raylib::Color(255, 255, 255, 255));
-        //ID_str.Draw(world2screen_X(obj_pos.x), world2screen_Y(obj_pos.y));
-           
-    }
- 
-    DrawFPS(10,10);
-
-}
-*/
-
 void Render_System_add_pre_render(void (*sys)(ECS_Manager&)){
     if (sys == nullptr){
         return; // Do Nothing
@@ -195,16 +115,7 @@ void Render_System(ECS_Manager &world){
         double rotation = (180.0/3.14159)*world.get_component<Rotation_Component>(it->entity_id)->angle;
         texture_ptr->Draw(src_rec, dest_rec, origin, -1*rotation); // Raylib has positive angles going
                                                                    // CW - I prefer the CCW - the way God intended.
-        /*
-        std::stringstream ss;
-        ss << std::fixed << std::setprecision(2) << rotation;
-        std::string mystring = ss.str();
-        raylib::Text ID_str(mystring, 16.0, raylib::Color(255, 255, 255, 255));
-        ID_str.Draw(x_pos, y_pos);
-        */  
     }
-
-    DrawFPS(10,10);
 
     call_post_render_systems(world);
     
