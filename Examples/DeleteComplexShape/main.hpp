68684b2da848f965a99d47baae92f9d1f172ca01
JoshuaBRussell <theruss007@gmail.com>
2024-10-06 20:01:54 -0400

    Add example showing a complex object having one of its links deleted. Add a function to the the re-initializes the Constraint system - must be called before the next call to the Constraint System's update function.

---
+++ Examples/DeleteComplexShape/main.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+
+#define SCREEN_WIDTH_IN_PIXELS  840
+#define SCREEN_HEIGHT_IN_PIXELS 840
+
+#define SCREEN_WIDTH_METERS  10.0
+#define SCREEN_HEIGHT_METERS 10.0
+
+#define OBJECT_RADIUS 0.1
